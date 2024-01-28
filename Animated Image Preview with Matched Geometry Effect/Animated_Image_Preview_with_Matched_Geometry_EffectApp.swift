b2f7c79166fc265161daa4881158fa7e169b5ca3
LATIFARYES <latifaryes@gmail.com>
2024-01-28 14:03:13 +0300

    Initial Commit

---
+++ Animated Image Preview with Matched Geometry Effect/Animated_Image_Preview_with_Matched_Geometry_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_Image_Preview_with_Matched_Geometry_EffectApp.swift
+//  Animated Image Preview with Matched Geometry Effect
+//
+//  Created by LATIFA on 28/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Animated_Image_Preview_with_Matched_Geometry_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated Image Preview with Matched Geometry Effect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animated Image Preview with Matched Geometry Effect
+//
+//  Created by LATIFA on 28/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
